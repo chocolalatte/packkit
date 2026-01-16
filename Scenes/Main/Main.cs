7b1994bd90d5aee5f13a8b0fd370e2b23d709f01
chocolalatte <iamduck18@gmail.com>
2026-01-16 16:36:00 -0500

    switched to TabContainer

--- Scenes/Main/Main.cs
+++ Scenes/Main/Main.cs
@@ -19,17 +19,4 @@ public partial class Main : Control
 
         PackManager.CreatePack(packName, "TestAuthor");
     }
-
-    private void _on_tab_bar_tab_changed(int index)
-    {
-        switch (index)
-        {
-            case 0:
-                PackList.Visible = false;
-                break;
-            case 1:
-                PackList.Visible = true;
-                break;
-        }
-    }
 }
